c69755eedcef3a8a3b2563b2036137f7d5982598
Melipilla <melipilla@duoc.acad>
2025-11-19 14:03:39 -0300

    se subio el proyecto al github

---
+++ demo/src/main/java/com/example/demo/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+import com.example.demo.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.Book;
+
+public interface BookRepository  extends JpaRepository<Book,Long> {
+}
